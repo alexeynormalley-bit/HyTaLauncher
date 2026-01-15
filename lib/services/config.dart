b28e0ad2fae712b3eb535586b033e80dfdcd7805
alexeynormalley-bit <alexeynormalley@gmail.com>
2026-01-16 00:37:35 +0500

    need

--- lib/services/config.dart
+++ lib/services/config.dart
@@ -1,6 +1,4 @@
-
 class Config {
-  static const String patchBaseUrl = "https://hytale-services.vercel.app/api/patch";
-  static const String curseForgeApiKey = ""; 
-  static const String onlineFixUrl = "https://github.com/Hytale-Fix-Repair/Hytale-Fix-Repair/releases/latest";
+  static const String patchBaseUrl = "https://game-patches.hytale.com/patches";
+  static const String curseForgeApiKey = "";
 }
